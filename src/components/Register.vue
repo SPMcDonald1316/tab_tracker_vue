7b8fed2ef2a483a44c8b70afbda5c7809b988a35
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-20 17:30:34 -0500

    add register functionality

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -4,23 +4,45 @@
     <input
       type="email"
       name="email"
+      v-model="email"
       placeholder="email"
     >
     <br>
     <input
       type="password"
       name="password"
+      v-model="password"
       placeholder="password"
     >
     <br>
-    <button>Register</button>
+    <button @click="register">Register</button>
   </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
   data () {
-    return {}
+    return {
+      email: '',
+      password: '',
+      errors: []
+    }
+  },
+  methods: {
+    register () {
+      const params = {
+        email: this.email,
+        password: this.password
+      }
+      axios.post('/api/users', params)
+        .then(response => {
+          this.$router.push('/login')
+        })
+        .catch(error => {
+          this.errors = error.response.data.errors
+        })
+    }
   }
 }
 </script>
