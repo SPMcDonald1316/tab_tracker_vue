2d6683e37ed5175cda6414ec648248979740b213
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-24 15:11:59 -0500

    add vuetify

--- src/main.js
+++ src/main.js
@@ -2,11 +2,15 @@ import Vue from 'vue'
 import App from './App.vue'
 import router from './router'
 import axios from 'axios'
+import Vuetify from 'vuetify'
+import 'vuetify/dist/vuetify.min.css'
 
 axios.defaults.baseURL =
   process.env.NODE_ENV === 'development' ? 'http://localhost:3000' : '/'
 
 Vue.config.productionTip = false
+
+Vue.use(Vuetify)
 
 new Vue({
   router,
