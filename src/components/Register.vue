9985f62c9cd28731c56ad9f6e666a9b0f34c9388
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-25 10:52:07 -0500

    add dark layout to register title

--- src/components/Register.vue
+++ src/components/Register.vue
@@ -2,7 +2,7 @@
   <v-layout column>
     <v-flex xs6 offset-xs3>
       <div class="white elevation-2">
-        <v-toolbar flat dense class="cyan">
+        <v-toolbar flat dense class="cyan" dark>
           <v-toolbar-title>Register</v-toolbar-title>
         </v-toolbar>
         <div class="pl-4 pr-4 pt-2 pb-2">
