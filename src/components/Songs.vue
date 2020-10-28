a4cbca47faa2daf05e06b0c8c3c4dd35ff866631
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-27 19:30:12 -0500

    create songs index component

--- src/components/Songs.vue
+++ src/components/Songs.vue
@@ -2,6 +2,8 @@
   <v-layout column justify-center>
     <v-flex>
       <panel title="Songs">
+        <div :key="song.id" v-for="song in songs">
+        </div>
       </panel>
     </v-flex>
   </v-layout>
@@ -9,9 +11,21 @@
 
 <script>
 import Panel from '@/components/Panel'
+import axios from 'axios'
 export default {
   components: {
     Panel
+  },
+  data () {
+    return {
+      songs: []
+    }
+  },
+  mounted () {
+    axios.get('/api/songs')
+      .then(response => {
+        this.songs = response.data.songs
+      })
   }
 }
 </script>
