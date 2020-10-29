87a6bcbc0e39370402009566f52bd5626d456cc6
Sean McDonald <spmcdonald1316@gmail.com>
2020-10-29 14:26:41 -0500

    fix add song component layout and code addSong method

--- src/components/AddSong.vue
+++ src/components/AddSong.vue
@@ -1,53 +1,69 @@
 <template>
-  <panel title="Song Metadata">
-    <form>
-      <v-text-field
-        label="title"
-        type="text"
-        v-model="title"
-      ></v-text-field>
-      <v-text-field
-        label="artist"
-        type="text"
-        v-model="artist"
-      ></v-text-field>
-      <v-text-field
-        label="genre"
-        type="text"
-        v-model="genre"
-      ></v-text-field>
-      <v-text-field
-        label="album"
-        type="text"
-        v-model="album"
-      ></v-text-field>
-      <v-text-field
-        label="albumImg"
-        type="text"
-        v-model="albumImg"
-      ></v-text-field>
-      <v-text-field
-        label="youtubeId"
-        type="text"
-        v-model="youtubeId"
-      ></v-text-field>
-      <v-text-field
-        label="lyrics"
-        type="textarea"
-        v-model="lyrics"
-      ></v-text-field>
-      <v-text-field
-        label="tab"
-        type="textarea"
-        v-model="tab"
-      ></v-text-field>
-      <v-btn class="cyan" dark>Add Song</v-btn>
-    </form>
-  </panel>
+  <v-layout>
+    <v-flex xs4>
+      <panel title="Song Metadata">
+        <form>
+          <v-text-field
+            label="Title"
+            type="text"
+            v-model="title"
+          ></v-text-field>
+          <v-text-field
+            label="Artist"
+            type="text"
+            v-model="artist"
+          ></v-text-field>
+          <v-text-field
+            label="Genre"
+            type="text"
+            v-model="genre"
+          ></v-text-field>
+          <v-text-field
+            label="Album"
+            type="text"
+            v-model="album"
+          ></v-text-field>
+          <v-text-field
+            label="Album Image Url"
+            type="text"
+            v-model="albumImg"
+          ></v-text-field>
+          <v-text-field
+            label="Youtube ID"
+            type="text"
+            v-model="youtubeId"
+          ></v-text-field>
+        </form>
+      </panel>
+    </v-flex>
+
+    <v-flex xs8>
+      <panel title="Lyrics and Tab">
+        <form>
+          <v-textarea
+            label="Lyrics"
+            type="text"
+            v-model="lyrics"
+          ></v-textarea>
+          <v-textarea
+            label="Tab"
+            type="text"
+            v-model="tab"
+          ></v-textarea>
+        </form>
+      </panel>
+      <v-btn
+        class="cyan"
+        dark
+        @click="addSong"
+      >Create Song</v-btn>
+    </v-flex>
+  </v-layout>
 </template>
 
 <script>
 import Panel from './Panel'
+import axios from 'axios'
 export default {
   data () {
     return {
@@ -58,11 +74,30 @@ export default {
       albumImg: '',
       youtubeId: '',
       lyrics: '',
-      tab: ''
+      tab: '',
+      errors: []
     }
   },
   components: {
     Panel
+  },
+  methods: {
+    addSong () {
+      const params = {
+        title: this.title,
+        artist: this.artist,
+        genre: this.genre,
+        album: this.album,
+        album_image: this.albumImg,
+        youtube_id: this.youtubeId,
+        lyrics: this.lyrics,
+        tab: this.tab
+      }
+      axios.post('/api/songs', params)
+        .catch(error => {
+          console.log(error)
+        })
+    }
   }
 }
 </script>
