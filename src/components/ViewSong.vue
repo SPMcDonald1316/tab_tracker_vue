b4f10fde2bf8cd9a68f0fa25e7d971e40c2139e7
Sean McDonald <spmcdonald1316@gmail.com>
2020-11-01 12:04:48 -0600

    started view song component

--- src/components/ViewSong.vue
+++ src/components/ViewSong.vue
@@ -1,10 +1,28 @@
 <template>
-  <p>Hello there!</p>
+  <div>
+    <p>{{song.title}}</p>
+    <p>{{song.artist}}</p>
+    <p>{{song.genre}}</p>
+  </div>
 </template>
 
 <script>
+import axios from 'axios'
 export default {
-
+  data () {
+    return {
+      song: null,
+      errors: []
+    }
+  },
+  created () {
+    const songId = this.$store.state.route.params.songId
+    axios.get(`/api/songs/${songId}`)
+      .then(response => {
+        console.log(response)
+        this.song = response.data
+      })
+  }
 }
 </script>
 
