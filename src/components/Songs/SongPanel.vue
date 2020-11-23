c7732885ba4fef330c5b6b7ad2373343296da4b7
Sean McDonald <spmcdonald1316@gmail.com>
2020-11-23 13:53:33 -0600

    create song panel component

---
+++ src/components/Songs/SongPanel.vue
@@ -0,0 +1,73 @@
+<template>
+  <panel title="Songs">
+    <v-btn
+      slot="action"
+      class="cyan accent-2"
+      light
+      small
+      absolute
+      right
+      middle
+      to='/songs/add'
+    ><v-icon>add</v-icon></v-btn>
+
+    <div class="song">
+      <v-layout>
+        <v-flex xs6>
+          <div class="song-title">
+            {{song.title}}
+          </div>
+          <div class="song-artist">
+            {{song.artist}}
+          </div>
+          <div class="song-genre">
+            {{song.genre}}
+          </div>
+
+          <v-btn
+            class="cyan"
+            dark
+            :to="{name: 'song', params: {songId: song.id}}"
+          >View</v-btn>
+        </v-flex>
+        <v-flex xs6>
+          <img class="album-image" :src="song.album_image" />
+        </v-flex>
+      </v-layout>
+    </div>
+  </panel>
+</template>
+
+<script>
+import Panel from '../Panel'
+export default {
+  props: [
+    'song'
+  ],
+  components: {
+    Panel
+  }
+}
+</script>
+
+<style scoped>
+  .song {
+    padding: 20px;
+    height: 330px;
+    overflow: hidden;
+  }
+  .song-title {
+    font-size: 30px;
+  }
+  .song-artist {
+    font-size: 24px;
+  }
+  .song-genre {
+    font-size: 18px;
+  }
+  .album-image {
+    width: 200px;
+    height: auto;
+    margin: 0 auto;
+  }
+</style>
--- src/components/Songs/Songs.vue
+++ src/components/Songs/Songs.vue
@@ -1,53 +1,17 @@
 <template>
   <v-layout column justify-center>
-    <v-flex>
-      <panel title="Songs">
-        <v-btn
-          slot="action"
-          class="cyan accent-2"
-          light
-          small
-          absolute
-          right
-          middle
-          to='/songs/add'
-        ><v-icon>add</v-icon></v-btn>
-
-        <div class="song" :key="song.id" v-for="song in songs">
-          <v-layout>
-            <v-flex xs6>
-              <div class="song-title">
-                {{song.title}}
-              </div>
-              <div class="song-artist">
-                {{song.artist}}
-              </div>
-              <div class="song-genre">
-                {{song.genre}}
-              </div>
-
-              <v-btn
-                class="cyan"
-                dark
-                :to="{name: 'song', params: {songId: song.id}}"
-              >View</v-btn>
-            </v-flex>
-            <v-flex xs6>
-              <img class="album-image" :src="song.album_image" />
-            </v-flex>
-          </v-layout>
-        </div>
-      </panel>
+    <v-flex :key="song.id" v-for="song in songs">
+      <song-panel :song="song"/>
     </v-flex>
   </v-layout>
 </template>
 
 <script>
-import Panel from '@/components/Panel'
+import SongPanel from './SongPanel'
 import axios from 'axios'
 export default {
   components: {
-    Panel
+    SongPanel
   },
   data () {
     return {
@@ -64,23 +28,4 @@ export default {
 </script>
 
 <style scoped>
-  .song {
-    padding: 20px;
-    height: 330px;
-    overflow: hidden;
-  }
-  .song-title {
-    font-size: 30px;
-  }
-  .song-artist {
-    font-size: 24px;
-  }
-  .song-genre {
-    font-size: 18px;
-  }
-  .album-image {
-    width: 200px;
-    height: auto;
-    margin: 0 auto;
-  }
 </style>
